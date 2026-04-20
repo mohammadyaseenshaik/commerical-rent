8f40e97edf3544af9fabea633bc6193930622f7d
Pushpesh Lingam <pushpeshlingam@gmail.com>
2026-04-20 11:54:12 +0530

    first commit

---
+++ backend/src/test/java/com/example/commerical_rent/CommericalRentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.commerical_rent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommericalRentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
