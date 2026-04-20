3a67644e5198f87fe82f4eb6d4f83d2b78f84383
mohammadyaseenshaik <mohammadyaseen_shaik@srmap.edu.in>
2026-04-20 14:21:42 +0530

    all user details commit

---
+++ backend/src/main/java/com/example/commerical_rent/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.example.commerical_rent.controller;
+
+import com.example.commerical_rent.entity.User;
+import com.example.commerical_rent.enums.Role;
+import com.example.commerical_rent.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        return ResponseEntity.ok(userService.createUser(user));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getUsers(@RequestParam(required = false) Role role) {
+        if (role != null) {
+            return ResponseEntity.ok(userService.getUsersByRole(role));
+        }
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+}
